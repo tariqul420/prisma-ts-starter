50124b04b5f270efecbc3e7f101a91652e034433
tariqul420 <tariqul.developer@gmail.com>
2025-07-05 10:00:08 +0600

    fix: update import path for PrismaClient to correct location

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './../../generated/prisma/index.d';
+import { PrismaClient } from './../../generated/prisma';
 const prisma = new PrismaClient();
 
 export default prisma;
