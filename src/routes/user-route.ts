c5fb5121cf36ff1887f683ad32ed63e319b22267
tariqul420 <tariqul.developer@gmail.com>
2025-07-05 09:05:19 +0600

    feat: implement user routes for creating, retrieving role, and updating users

---
+++ src/routes/user-route.ts
@@ -0,0 +1,13 @@
+import { RequestHandler, Router } from 'express';
+import {
+  createUser,
+  getUserRole,
+  updateUser,
+} from '../controllers/user-controller';
+
+const router = Router();
+
+// user routers
+router.post('/create', createUser as RequestHandler);
+router.get('/role/:email', getUserRole as RequestHandler);
+router.put('/update/:email', updateUser as RequestHandler);
