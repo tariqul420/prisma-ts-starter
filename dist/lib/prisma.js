a392fe0875d38a3fb97aaf2ac8d94372098cb145
tariqul420 <tariqul.developer@gmail.com>
2025-07-06 13:29:28 +0600

    refactor: remove dist folder and clean up generated files

--- dist/lib/prisma.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_d_1 = require("./../../generated/prisma/index.d");
-const prisma = new index_d_1.PrismaClient();
-exports.default = prisma;
